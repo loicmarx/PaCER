b9b36a6a9fc11d18bed936723fc2837a5829ee3c
Loic Marx <loic.marx@gmail.com>
2019-05-28 16:23:54 +0200

    WIP comparison of different structures of testElectrodes

--- test/suite/testElectrodes.m
+++ test/suite/testElectrodes.m
@@ -16,8 +16,27 @@ cd(fileDir);
 % load the reference data into the test file
 refData = load('refData_testElectrodes.mat');
 
-% generate new outputs
-testElectrodes_new = TestElectrodes();
+% compare if the structures have been correctly defined  
+assert(testElectrodes_ref.obliqueViewRadius() == 80)
+%assert(testElectrodes_ref.originalEntryPoint() == isnan(NaN(3,1)))
+assert(testElectrodes_ref.marker() == 'x')
+assert(testElectrodes_ref.diameter()== 4 )
+%assert(testElectrodes_ref.lineStyle() == logical('--'))
+%assert(testElectrodes_ref.color() == [0.2 1 0.4])
+assert(testElectrodes_ref.show2DProjection() == 1)
+assert(testElectrodes_ref.currentDepthValue() == 0)
+%assert(testElectrodes_ref.intersections() == zeros(2,5)) 
+assert(testElectrodes_ref.ELECTRODE_DISTANCE() == 2.0 ) 
+assert(testElectrodes_ref.ELECTRODE_RECORDED_INTERSECTION_COLOR() == 'r') 
+assert(testElectrodes_ref.ELECTRODE_INTERSECTION_COLOR() == 'g') 
+%assert(testElectrodes_ref.ELECTRODE_COLOR() == [0 1 1] ) 
+assert(testElectrodes_ref.ELECTRODE_PLOT_EXTENSION_MM() == 15)
+assert(testElectrodes_ref.ELECTRODE_INTERSECTION_COLOR() == 'g' )
+assert(testElectrodes_ref.DISPLAY_TARGET_POINTS() == false)
+assert(testElectrodes_ref.objectId() == 44)
 
 
+%get.electrodesEntryPoints(testElectrodes_ref)
+
+%assert(isequal(testElectrodes_ref.obliqueViewRadius, testElectrodes_new.obliqueViewRadius))
 
