ccc4cff0d61787c2c8d20b7962fc3650577d51a9
Loic Marx <loic.marx@gmail.com>
2019-05-28 18:08:59 +0200

    implement the function

--- test/suite/structureComparison.m
+++ test/suite/structureComparison.m
@@ -17,12 +17,25 @@ function structureComparison(structure_ref, structure_new)
 % .. AUTHOR:
 %       - Loic MARX, MAY 2019
 
-    for j=1:length(structure_ref)
-        fn = fieldnames(structure_ref{j});
-        for k = 1:length(fn) 
-            if (~isnumeric(getfield(structure_new{j}, fn{k})) && ~isnumeric(getfield(structure_ref{j}, fn{k})))
-                assert(isequal(getfield(structure_new{j}, fn{k}), getfield(structure_ref{j}, fn{k})))
+    if length(structure_ref) > 1
+        for j=1:length(structure_ref)
+            fn = fieldnames(structure_ref{j});
+            for k = 1:length(fn) 
+                if (~isnumeric(getfield(structure_new{j}, fn{k})) && ~isnumeric(getfield(structure_ref{j}, fn{k})))
+                    assert(isequal(getfield(structure_new{j}, fn{k}), getfield(structure_ref{j}, fn{k})))
+                end
             end
         end
+    else
+        fn = fieldnames(structure_ref);
+        keyboard
+            for k = 1:length(fn)
+                k
+                fn(k)
+                if (~isnumeric(getfield(structure_new, fn{k})) && ~isobject(getfield(structure_new, fn{k})) && ~isnumeric(getfield(structure_ref, fn{k}))) && ~isobject(getfield(structure_ref, fn{k}))
+
+                    assert(isequal(getfield(structure_new, fn{k}), getfield(structure_ref, fn{k})))
+                end
+            end
     end
 end
