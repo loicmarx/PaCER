626f44611eb97dd8e5323ef5f5ba14a4cc30efe3
Loic Marx <loic.marx@gmail.com>
2019-07-06 11:04:52 +0200

    generation of new output with three inputs arguments

--- test/suite/testRefitElec.m
+++ test/suite/testRefitElec.m
@@ -15,3 +15,21 @@ currentDir = pwd;
 % initialize the test
 fileDir = fileparts(which(mfilename));
 cd(fileDir);
+
+% input argument 
+inputs = load('input_refitElec.mat');
+
+% load the reference data
+refData = load('refData_testRefitElec.mat');
+
+% setup tolerance value for comparison
+tol = 10-6; 
+
+% generate the new output (only three inputs argument)
+[refitReZeroedElecMod_new, filteredIntensity_new, skelScaleMm_new] = refitElec(inputs.initialPoly, inputs.pointCloudWorld, inputs.voxelValues);
+
+% compare the reference data with the generated output 
+
+
+
+
